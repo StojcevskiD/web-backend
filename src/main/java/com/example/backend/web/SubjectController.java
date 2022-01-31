6de93168018305caa951fcd8abbce3e4aaa325c3
StojcevskiD <dimitar.stojcevski@hotmail.com>
2022-01-31 16:24:57 +0100

    Environment done

--- src/main/java/com/example/backend/web/SubjectController.java
+++ src/main/java/com/example/backend/web/SubjectController.java
@@ -2,17 +2,21 @@ package com.example.backend.web;
 
 import com.example.backend.model.Subject;
 import com.example.backend.service.interfaces.SubjectService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
+@CrossOrigin
 @RestController
 @RequestMapping("/subject")
 public class SubjectController {
     private final SubjectService subjectService;
 
+//    @Autowired
     public SubjectController(SubjectService subjectService) {
         this.subjectService = subjectService;
     }
