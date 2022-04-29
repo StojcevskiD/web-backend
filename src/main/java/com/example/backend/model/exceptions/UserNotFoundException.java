19187d923234efa0a1eb1484afaeefec83309d28
Jovana <jsmilevska@yahoo.com>
2022-04-29 17:16:10 +0200

    refactor

--- src/main/java/com/example/backend/model/exceptions/UserNotFoundException.java
+++ src/main/java/com/example/backend/model/exceptions/UserNotFoundException.java
@@ -3,8 +3,6 @@ package com.example.backend.model.exceptions;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
-import java.util.function.Supplier;
-
 @ResponseStatus(code = HttpStatus.NOT_FOUND)
 public class UserNotFoundException extends RuntimeException{
     public UserNotFoundException(){
